503c606c3f23f3db795c675fcb4bc5438a7739da
Bram Borggreve <beeman@beeman.nl>
2025-01-15 14:15:09 +0000

    fix: make ensureIdentityOwner message more specific

--- libs/sdk/src/generated/graphql-sdk.ts
+++ libs/sdk/src/generated/graphql-sdk.ts
@@ -209,7 +209,7 @@ export type AppConfig = {
   appThemeColor?: Maybe<Scalars['String']['output']>
   authLinkProviders?: Maybe<Array<IdentityProvider>>
   authLoginProviders?: Maybe<Array<IdentityProvider>>
-  authTelegramBotName?: Maybe<Scalars['String']['output']>
+  authTelegramBotName: Scalars['String']['output']
   features: Array<AppFeature>
   resolvers: Array<NetworkResolver>
 }
@@ -4170,7 +4170,7 @@ export type AppConfigDetailsFragment = {
   appThemeColor?: string | null
   authLinkProviders?: Array<IdentityProvider> | null
   authLoginProviders?: Array<IdentityProvider> | null
-  authTelegramBotName?: string | null
+  authTelegramBotName: string
   features: Array<AppFeature>
   resolvers: Array<NetworkResolver>
 }
@@ -4202,7 +4202,7 @@ export type AppConfigQuery = {
     appThemeColor?: string | null
     authLinkProviders?: Array<IdentityProvider> | null
     authLoginProviders?: Array<IdentityProvider> | null
-    authTelegramBotName?: string | null
+    authTelegramBotName: string
     features: Array<AppFeature>
     resolvers: Array<NetworkResolver>
   }
